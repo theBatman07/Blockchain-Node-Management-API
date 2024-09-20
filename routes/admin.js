9bd79f36302e7d064e3bb2d500a1202ada4b40f7
Gaurav Khater <85270515+theBatman07@users.noreply.github.com>
2024-09-21 00:21:32 +0530

    updated admin functionality

--- routes/admin.js
+++ routes/admin.js
@@ -3,10 +3,16 @@ const router = express.Router();
 const auth = require('../middleware/auth');
 const roleAuth = require('../middleware/roleAuth');
 const { storeNode, getNodes, updateNode, deleteNode } = require('../controllers/adminController');
+const { storeMachine, getMachines, updateMachine, deleteMachine } = require('../controllers/providerController');
 
 router.post('/nodes', [auth, roleAuth(['admin'])], storeNode);
 router.get('/nodes', [auth, roleAuth(['admin'])], getNodes);
 router.put('/nodes/:id', [auth, roleAuth(['admin'])], updateNode);
 router.delete('/nodes/:id', [auth, roleAuth(['admin'])], deleteNode);
+
+router.post('/machines', [auth, roleAuth(['admin'])], storeMachine);
+router.get('/machines', [auth, roleAuth(['admin'])], getMachines);
+router.put('/machines/:id', [auth, roleAuth(['admin'])], updateMachine);
+router.delete('/machines/:id', [auth, roleAuth(['admin'])], deleteMachine);
 
 module.exports = router;
